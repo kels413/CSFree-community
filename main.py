463ce64a73d5eed0c2a0aed6bbba0374a4cd7ac8
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-22 13:21:18 +0100

    teaching

---
+++ main.py
@@ -0,0 +1,35 @@
+# import sys
+
+# # Create an object
+# my_object = "Hello, World!"
+
+# # Get the reference count
+# ref_count = sys.getrefcount(my_object)
+
+# # Print the reference count
+# print(f"Reference count: {ref_count}")
+
+
+# a = 100
+# b = 100
+# c = "kelly"
+# c = 100
+# c = 40
+
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# print(id(c))
+# print(id(c))
+
+# print(a is b)
+
+
+x = 'hi!'
+y = 'hi!'
+
+print(x == y)
+
+
+
+y
