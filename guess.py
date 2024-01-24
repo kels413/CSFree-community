89a8a0df94374f6ef4b87bc5074bf02dbc13c79c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-24 17:24:08 +0100

    working

--- guess.py
+++ guess.py
@@ -31,10 +31,12 @@ while True:
             user_input = int(input("what's your choice "))
             if user_input == secret_code:
                 print("you won")
+                user_higher_score += 1
                 exit(0)
         else:
             print("bot won")
             exit(0)
     else:
         print("you won")
+        user_higher_score += 1
         exit(0)
