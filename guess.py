7301f5bb14192d8b1c5a5c9ddb4eb7eb75e84d22
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-24 22:41:17 +0100

    working

--- guess.py
+++ guess.py
@@ -1,59 +1,33 @@
-"""Educational guess the secret code game in python"""
-# generate random numbers for bot choice and secret code
+"""A Guess the secret code game"""
+
+print("A guess game ")
+
 import random
 import time
-
-while True:
+while  True:
     secret_code = random.randint(1, 9)
     bot_choice = random.randint(1, 9)
+
     print(secret_code)
     print(bot_choice)
-    bot_message = ["oh dear, you missed the mark!❌ Let me show you how it's done.💯",
-                   "Nice try, human!🧔 Now, watch📺 and learn.📝",
-                   "Oh, almost there 😹, but not quite! Allow me to demonstrate precision. 🏹",
-                   "A valiant effort😡, but no luck this time😹! Time for the bot to shine.💃🏽"
-                   ]
-    user_higher_score = 0
-    bot_higher_score = 0
+    user_input = input("Your Choice: ")
 
-    user_input = int(input("what's your choice "))
+    try:
+        user_input = int(user_input)
+    except Exception:
+        user_input = input("Your Choice: ")
 
     while True:
-        bot_message_random = random.randint(0, 3)
-        bot_messages = bot_message[bot_message_random]
-        if user_input != secret_code and bot_choice != secret_code:
-            print(bot_messages)
-            print("Bot Thinking 🤔...")
-            time.sleep(1)
-            print("Bot Thinking 🤔...")
-            time.sleep(1)
-            print(f"Wrong choice: {bot_choice} 🤖")
-            user_input = int(input("what's your choice "))
+        if user_input == secret_code:
+            print("user won")
+            if bot_choice in range(1, 5):
+                print("im not afraid man, keep it rolling")
         elif bot_choice == secret_code:
-            print(bot_messages)
-            print("Bot Thinking 🤔...")
-            time.sleep(1)
-            print("Bot Thinking 🤔...")
-            time.sleep(1)
-            print(f"Bot picked {bot_choice}")
-            print(f"🤖Told you I got this man: 💃🏽")
-            print("bot won")
+            print("bot Won")
+  
             break
-
         else:
-            print("you won")
-            user_higher_score += 1
-            break
-    play_again = input("Do you want to play again? you bot ? 🤖: ")
-    time.sleep(1)
-    print("Bot Thinking 🤔...")
-    time.sleep(1)
-    print("Bot Thinking 🤔...")
-    if bot_choice not in range(1, 5):
-        print("can't play right now")
-        break
-    else:
-        print("Never afraid of you man, keep it coming")
-        continue
+            print("you lost")
+
 
 
