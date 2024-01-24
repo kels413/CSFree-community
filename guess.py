f2137c1ada9a045dd7f2683a2f808930ea020d2c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-24 17:31:48 +0100

    updated the game flow

--- guess.py
+++ guess.py
@@ -6,7 +6,7 @@ import time
 secret_code = random.randint(1, 9)
 bot_choice = random.randint(1, 9)
 print(secret_code)
-
+print(bot_choice)
 bot_message = ["oh dear, you missed the mark!❌ Let me show you how it's done.💯",
                "Nice try, human!🧔 Now, watch📺 and learn.📝",
                "Oh, almost there 😹, but not quite! Allow me to demonstrate precision. 🏹",
@@ -20,22 +20,30 @@ bot_messages = bot_message[bot_message_random]
 user_input = int(input("what's your choice "))
 
 while True:
-    if user_input != secret_code:
+    if user_input != secret_code and bot_choice != secret_code:
         print(bot_messages)
         print("Bot Thinking 🤔...")
         time.sleep(1)
         print("Bot Thinking 🤔...")
         time.sleep(1)
-        if bot_choice != secret_code:
-            print(f"Wrong choice: {bot_choice} 🤖")
-            user_input = int(input("what's your choice "))
-            if user_input == secret_code:
-                print("you won")
-                user_higher_score += 1
-                exit(0)
-        else:
-            print("bot won")
-            exit(0)
+        print(f"Wrong choice: {bot_choice} 🤖")
+        user_input = int(input("what's your choice "))
+        # if bot_choice != secret_code:
+        #     print(f"Wrong choice: {bot_choice} 🤖")
+        #     user_input = int(input("what's your choice "))
+        #     if user_input == secret_code:
+        #         print("you won")
+        #         user_higher_score += 1
+        #         exit(0)
+    elif bot_choice == secret_code:
+        print(bot_messages)
+        print("Bot Thinking 🤔...")
+        time.sleep(1)
+        print("Bot Thinking 🤔...")
+        time.sleep(1)
+        print(f"🤖Told you I got this man: {bot_choice} 💃🏽")
+        print("bot won")
+        exit(0)
     else:
         print("you won")
         user_higher_score += 1
